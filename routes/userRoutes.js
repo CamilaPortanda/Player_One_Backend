d864afd3510d9970756e266c57d99250b6a194bb
Valeria Rosado <a00841102@tec.mx>
2026-05-24 20:17:02 -0600

    ProfilePicsEndpoints

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const verificarToken = require('../middleware/verificarToken');
 
 router.get('/perfil', verificarToken, async (req, res) => {
   const usuario = await User.findByPk(req.usuario.user_id, {
-    attributes: ['user_id', 'first_name', 'last_name', 'email', 'phone', 'industry', 'company', 'job_position']
+    attributes: ['user_id', 'first_name', 'last_name', 'email', 'phone', 'industry', 'company', 'job_position','pfp_id']
   });
   if (!usuario) {
     return res.status(404).json({ error: 'Usuario no encontrado' });
